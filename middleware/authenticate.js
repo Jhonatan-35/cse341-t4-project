00c62673fa231f755bde32c529f4eb1a3c98ad21
RBennyjay <ejohn1@byupathway.edu>
2025-08-13 14:23:55 +0100

    updated your isAuthenticated middleware function

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,13 +1,16 @@
+// In file: ../middleware/authenticate.js
+
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user === undefined) {
-        console.log("User not authenticated");
-        res.status(401).json({
-            message: "You do not have access"
-        });
+    console.log('Checking authentication for protected route...');
+    console.log('req.user after deserialization:', req.user);
+    
+    if (req.isAuthenticated()) {
+        console.log('User is authenticated!');
+        return next();
     } else {
-        console.log("User authenticated");
-        next();
+        console.log('User is NOT authenticated. req.isAuthenticated() returned false.');
+        res.status(401).json({ message: 'You do not have access' });
     }
-}
+};
 
 module.exports = isAuthenticated;
