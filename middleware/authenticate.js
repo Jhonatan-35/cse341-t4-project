bc967bec478943a6c663b0fcfcb1e3d4fef0b703
RBennyjay <ejohn1@byupathway.edu>
2025-08-13 15:09:23 +0100

    Removing  the diagnostic logging  to authenticate.js file

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,14 +1,9 @@
 // In file: ../middleware/authenticate.js
 
 const isAuthenticated = (req, res, next) => {
-    console.log('Checking authentication for protected route...');
-    console.log('req.user after deserialization:', req.user);
-    
     if (req.isAuthenticated()) {
-        console.log('User is authenticated!');
         return next();
     } else {
-        console.log('User is NOT authenticated. req.isAuthenticated() returned false.');
         res.status(401).json({ message: 'You do not have access' });
     }
 };
